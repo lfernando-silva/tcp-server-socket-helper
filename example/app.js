ca08a56429353e7e1e1b6ad7574fa5178553cfbb
Luiz Fernando <lfernando-silva@hotmail.com>
2016-11-28 14:25:01 -0200

    Add files via upload

---
+++ example/app.js
@@ -0,0 +1,25 @@
+﻿const ServerSocket = require('../lib/ServerSocket.js');
+
+//Set event handlers 
+let events = {
+    'data': (data) => { },
+    'error': (err) => { },
+    'timeout': () => { },
+    'close': () => { },
+    'end': () => { }
+}
+
+//Set options
+let options = {
+    timeout: 1030000,
+    keepAlive: {
+        enabled: false,
+        initialDelay: 30
+    }
+}
+
+//Create a new server
+const Server = new ServerSocket('localhost', 4100, events, options);
+
+//Start listening
+Server.start();
