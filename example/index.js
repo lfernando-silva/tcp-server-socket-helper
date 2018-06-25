3a135ba988f90f808c683cb308affeca89cfff29
Luiz Fernando <lfernando-silva@hotmail.com>
2018-06-25 19:51:27 -0300

    Refactoring

---
+++ example/index.js
@@ -0,0 +1,25 @@
+﻿const Server = require('../lib/ServerSocket.js');
+
+const definitions = {
+    server: {
+      port: process.env.PORT || '4100',
+      host: process.env.HOST || 'localhost'
+    },
+    socket: {
+        timeout: 1030000,
+        keepAliveEnabled: false,
+        keepAliveDelay: 30
+    },
+};
+
+const handlers = {
+    onData: (socket) => data => socket.write(data.toString().toUpperCase()),
+    onError: socket => err => socket.write('Error'),
+    onTimeout: socket => () => socket.write('Timeout'),
+    onClose: socket => () => socket.write('Close'),
+    onEnd: socket => () => socket.write('End'),
+    onConnect: socket => () => socket.write('Connected'),
+};
+
+//Start listening
+Server.start({definitions, handlers});
